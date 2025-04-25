e410e7272b92fdc69b85d8d893bf680f545f25c3
xlan-xlan <xlan@ucdavis.edu>
2025-04-24 23:47:28 -0700

    Add files via upload

--- sshell.c
+++ sshell.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>  
-#include <sys/wait.h>    
-#include <fcntl.h>      
+#include <sys/types.h>   // for pid_t
+#include <sys/wait.h>    // for waitpid() and WEXITSTATUS()
+#include <fcntl.h>       // for open(), O_CREAT, O_TRUNC
 
 #define CMDLINE_MAX    512
 #define MAX_ARGUMENT   16
 #define MAX_PIPES       3
-#define MAX_COMMANDS    4
+#define MAX_COMMANDS   (MAX_PIPES + 1)
 
 typedef struct {
     char *argv[MAX_ARGUMENT + 1];
@@ -18,16 +18,16 @@ typedef struct {
 
 typedef struct {
     Command cmds[MAX_COMMANDS];
-    int commandd;
-    int Outre;  
-    int Inre;   
+    int commands;
+    int outputRed;   
+    int InputRed;  
     char *outfile;  
-    char *infile; 
-    int background;    
+    char *infile;  
+    int background;     
 } Job;
 
-// cd
-int cd(char *args[]) {
+
+int cd_build(char *args[]) {
     if (!args[1]) {
         fprintf(stderr, "Error: no directory specified\n");
         return 1;
@@ -40,24 +40,24 @@ int cd(char *args[]) {
 }
 
 
-void padd(char *cmd) {
-    char copy[CMDLINE_MAX];
-    int r = 0, w = 0;
-    while (cmd[r] && w < CMDLINE_MAX - 1) {
-        if (cmd[r] == '>' || cmd[r] == '|' || cmd[r] =='<' || cmd[r] =='&') {
-            copy[w++] = ' ';
-            copy[w++] = cmd[r++];
-            copy[w++] = ' ';
+void pad(char *cmd) {
+    char copyc[CMDLINE_MAX];
+    int firstI = 0, SecondI = 0;
+    while (cmd[firstI] && SecondI < CMDLINE_MAX - 1) {
+        if (cmd[firstI] == '>' || cmd[firstI] == '|' || cmd[firstI] =='<' || cmd[firstI] =='&') {
+            copyc[SecondI++] = ' ';
+            copyc[SecondI++] = cmd[firstI++];
+            copyc[SecondI++] = ' ';
         } else {
-            copy[w++] = cmd[r++];
+            copyc[SecondI++] = cmd[firstI++];
         }
     }
-    copy[w] = '\0';
-    strncpy(cmd, copy, CMDLINE_MAX);
+    copyc[SecondI] = '\0';
+    strncpy(cmd, copyc, CMDLINE_MAX);
 }
 
 
-void OutRedirection(const char *outfile) {
+void Outputredirection(const char *outfile) {
     int fd = open(outfile, O_WRONLY|O_CREAT|O_TRUNC, 0644);
     if (fd < 0) {
         fprintf(stderr, "Error: cannot open output file\n");
@@ -70,7 +70,7 @@ void OutRedirection(const char *outfile) {
     close(fd);
 }
 
-void InputRedirection(const char *infile){
+void Inputredirection(const char *infile){
     int fd = open(infile, O_RDONLY);
     if (fd < 0) {
         fprintf(stderr, "Error: cannot open input file\n");
@@ -83,8 +83,8 @@ void InputRedirection(const char *infile){
     close(fd);
 }
 
-int Parseall(char *line, Job *job) {
-    padd(line);
+int ParseAll(char *line, Job *job) {
+    pad(line);
     char *tokens[CMDLINE_MAX];
     int ntok = 0;
     char *t = strtok(line, " ");
@@ -93,7 +93,7 @@ int Parseall(char *line, Job *job) {
         t = strtok(NULL, " ");
     }
     if (ntok == 0) {
-        job->commandd = 0;
+        job->commands = 0;
         return 0;
     }
     for (int i = 0; i < ntok; i++) {
@@ -112,11 +112,11 @@ int Parseall(char *line, Job *job) {
         return -1;
     }
 
-   
+  
     job->background = 0;
     if (ntok > 0 && strcmp(tokens[ntok - 1], "&") == 0) {
         job->background = 1;
-        ntok--;
+        ntok--; 
         if (ntok == 0) {
             fprintf(stderr, "Error: missing command\n");
             return -1;
@@ -124,82 +124,84 @@ int Parseall(char *line, Job *job) {
     }    
 
 
-    job->Outre = 0;
+    job->outputRed = 0;
     job->outfile = NULL;
-    int OutRedirectionCheck = -1;
+    int OutRedChecker = -1;
     for (int i = 0; i < ntok; i++) {
         if (strcmp(tokens[i], ">") == 0) {
-            OutRedirectionCheck = i;
+            OutRedChecker = i;
             break;
         }
     }
-    if (OutRedirectionCheck >= 0) {
-        if (OutRedirectionCheck == 0) {
+    if (OutRedChecker >= 0) {
+        if (OutRedChecker == 0) {
             fprintf(stderr, "Error: missing command\n");
             return -1;
         }
-        if (OutRedirectionCheck == ntok - 1) {
+        if (OutRedChecker == ntok - 1) {
             fprintf(stderr, "Error: no output file\n");
             return -1;
         }
-        job->Outre = 1;
-        job->outfile  = tokens[OutRedirectionCheck + 1];
+        job->outputRed = 1;
+        job->outfile  = tokens[OutRedChecker + 1];
       
-        for (int j = OutRedirectionCheck + 2; j < ntok; j++) {
+        for (int j = OutRedChecker + 2; j < ntok; j++) {
             if (strcmp(tokens[j], "|") == 0) {
                 fprintf(stderr, "Error: mislocated output redirection\n");
-                return -1; 
+                return -1;
+            }
         }
-       
-        for (int j = OutRedirectionCheck; j + 2 < ntok; j++) {
+      
+        for (int j = OutRedChecker; j + 2 < ntok; j++) {
             tokens[j] = tokens[j + 2];
         }
         ntok -= 2;
     }
 
-   
-    job->Inre = 0;
+  
+    job->InputRed = 0;
     job->infile = NULL;
-    int InRedirectionchecker = -1;
+    int InputRedChecker = -1;
     for (int i = 0; i < ntok; i++) {
         if (strcmp(tokens[i], "<") == 0) {
-            InRedirectionchecker = i;
+            InputRedChecker = i;
             break;
         }
     }
-    if (InRedirectionchecker >= 0) {
-        if (InRedirectionchecker == 0) {
+    if (InputRedChecker >= 0) {
+        if (InputRedChecker == 0) {
             fprintf(stderr, "Error: missing command\n");
             return -1;
         }
-        if (InRedirectionchecker == ntok - 1) {
+        if (InputRedChecker == ntok - 1) {
             fprintf(stderr, "Error: no input file\n");
             return -1;
         }
-        
-        for (int j = 0; j < InRedirectionchecker; j++) {
+        // checking for input redirection after a "|"
+        for (int j = 0; j < InputRedChecker; j++) {
             if (strcmp(tokens[j], "|") == 0) {
                 fprintf(stderr, "Error: mislocated input redirection\n");
                 return -1;
             }
         }
-        job->Inre = 1;
-        job->infile = tokens[InRedirectionchecker + 1];
-     
-        for (int j = InRedirectionchecker + 2; j < ntok; j++) {
+        job->InputRed = 1;
+        job->infile = tokens[InputRedChecker + 1];
+    
+        for (int j = InputRedChecker + 2; j < ntok; j++) {
             if (strcmp(tokens[j], "|") == 0) {
                 fprintf(stderr, "Error: mislocated input redirection\n");
                 return -1;
             }
         }
-  
-        for (int j = InRedirectionchecker; j + 2 < ntok; j++) {
+   
+        for (int j = InputRedChecker; j + 2 < ntok; j++) {
             tokens[j] = tokens[j + 2];
         }
         ntok -= 2;
     }
 
-    int pipe_pos[MAX_PIPES];
+    // detect pipes
+    int pipev[MAX_PIPES];
     int pipecount = 0;
     for (int i = 0; i < ntok; i++) {
         if (strcmp(tokens[i], "|") == 0) {
@@ -207,27 +209,27 @@ int Parseall(char *line, Job *job) {
                 fprintf(stderr, "Error: too many pipes\n");
                 return -1;
             } 
-            pipe_pos[pipecount++] = i;
+            pipev[pipecount++] = i;
         }  
     }
 
     for (int p = 0; p < pipecount; p++) {
-        int pos = pipe_pos[p];
+        int pos = pipev[p];
         if (pos == 0 || pos == ntok - 1) {
             fprintf(stderr, "Error: missing command\n");
             return -1;
         }
-        if (p + 1 < pipecount && pipe_pos[p + 1] == pos + 1) {
+        if (p + 1 < pipecount && pipev[p + 1] == pos + 1) {
             fprintf(stderr, "Error: missing command\n");
             return -1;
         }
     }
 
-
-    job->commandd = pipecount + 1;
+   
+    job->commands = pipecount + 1;
     int start = 0;
-    for (int c = 0; c < job->commandd; c++) {
-        int end = (c < pipecount ? pipe_pos[c] : ntok);
+    for (int c = 0; c < job->commands; c++) {
+        int end = (c < pipecount ? pipev[c] : ntok);
         int argc = end - start;
         for (int j = 0; j < argc; j++) {
             job->cmds[c].argv[j] = tokens[start + j];
@@ -240,11 +242,11 @@ int Parseall(char *line, Job *job) {
     return 0;
 }
 
-void Executeall(const Job *job, const char *cmdBuffer) {
-    int n = job->commandd;
+void executionALL(const Job *job, const char *cmdBuffer) {
+    int n = job->commands;
     if (n == 0) return;
 
-   
+  
     if (n == 1) {
         pid_t pid = fork();
         if (pid < 0) {
@@ -252,12 +254,12 @@ void Executeall(const Job *job, const char *cmdBuffer) {
             return;
         }
         if (pid == 0) {
-            if (job->Outre) {
-             
-                OutRedirection(job->outfile);
+            if (job->outputRed) {
+              
+                Outputredirection(job->outfile);
             }
-            if (job->Inre){
-                InputRedirection(job->infile);
+            if (job->InputRed){
+                Inputredirection(job->infile);
             }
             execvp(job->cmds[0].argv[0], job->cmds[0].argv);
             fprintf(stderr, "Error: command not found\n");
@@ -266,12 +268,21 @@ void Executeall(const Job *job, const char *cmdBuffer) {
         
         int status;
         waitpid(pid, &status, 0);
-        fprintf(stderr, "+ completed '%s' [%d]\n", cmdBuffer, WEXITSTATUS(status));
+        if (WEXITSTATUS(status) == 0){
+            fprintf(stderr, "+ completed '%s' [%d]\n", cmdBuffer, WEXITSTATUS(status));
+        }
 
+        /*if (job->background) {
+            fprintf(stderr, "+ background pid: %d\n", pid);
+        } else {
+            int status;
+            waitpid(pid, &status, 0);
+            fprintf(stderr, "+ completed '%s' [%d]\n", cmdBuffer, WEXITSTATUS(status));
+        }*/
         return;
     }
 
-  
+
     int pipes[MAX_PIPES][2];
     for (int i = 0; i < n - 1; i++) {
         if (pipe(pipes[i]) < 0) {
@@ -281,7 +292,6 @@ void Executeall(const Job *job, const char *cmdBuffer) {
     }
 
     pid_t pids[MAX_COMMANDS];
- 
     for (int i = 0; i < n; i++) {
         pid_t pid = fork();
         if (pid < 0) {
@@ -289,18 +299,18 @@ void Executeall(const Job *job, const char *cmdBuffer) {
             exit(1);
         }
         if (pid == 0) {
+            // child i
             if (i > 0) {
                 dup2(pipes[i - 1][0], STDIN_FILENO);
             }
             if (i < n - 1) {
                 dup2(pipes[i][1], STDOUT_FILENO);
-            } else if (job->Outre) {
-                OutRedirection(job->outfile);
+            } else if (job->outputRed) {
+                Outputredirection(job->outfile);
             }
-            if (i == 0 && job-> Inre) {
-                InputRedirection(job->infile);
+            if (i == 0 && job-> InputRed) {
+                Inputredirection(job->infile);
             }
-          
             for (int j = 0; j < n - 1; j++) {
                 close(pipes[j][0]);
                 close(pipes[j][1]);
@@ -312,22 +322,27 @@ void Executeall(const Job *job, const char *cmdBuffer) {
         pids[i] = pid;
     }
 
- 
     for (int i = 0; i < n - 1; i++) {
         close(pipes[i][0]);
         close(pipes[i][1]);
     }
 
-   
+
     int statuses[MAX_COMMANDS];
     for (int i = 0; i < n; i++) {
         int st;
         waitpid(pids[i], &st, 0);
         statuses[i] = WEXITSTATUS(st);
-
+        /*if (job->background) {
+            fprintf(stderr, "+ background pid: %d\n", pids[n - 1]);  // or track all if needed
+        } else {
+            int st;
+            for (int i = 0; i < n; i++) {
+                waitpid(pids[i], &st, 0);
+            }
+            fprintf(stderr, "+ completed '%s' [%d]\n", cmdBuffer, WEXITSTATUS(st));
+        }*/
     }
-
-
     fprintf(stderr, "+ completed '%s'", cmdBuffer);
     for (int i = 0; i < n; i++) {
         fprintf(stderr, " [%d]", statuses[i]);
@@ -374,24 +389,21 @@ int main(void) {
         }
 
         Job job;
-        if (Parseall(cmd, &job) != 0) {
-          
+        if (ParseAll(cmd, &job) != 0) {
             continue;
         }
-        if (job.commandd == 0) {
-        
+        if (job.commands == 0) {
             continue;
         }
-     
-        if (job.commandd == 1 &&
+        if (job.commands == 1 &&
             strcmp(job.cmds[0].argv[0], "cd") == 0) {
-            int st = cd(job.cmds[0].argv);
+            int st = cd_build(job.cmds[0].argv);
             fprintf(stderr, "+ completed '%s' [%d]\n",
                     cmdBuffer, st);
             continue;
         }
 
-        Executeall(&job, cmdBuffer);
+        executionALL(&job, cmdBuffer);
     }
 
     return EXIT_SUCCESS;
